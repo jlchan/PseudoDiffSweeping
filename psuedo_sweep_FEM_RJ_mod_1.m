841d09f43dfd098be76aeead2809eb530f5bba2e
Jesse Chan <jesse.chan@rice.edu>
2022-06-09 11:34:14 -0500

    start modifying sweeping code to add FE

--- psuedo_sweep_FEM_RJ_mod_1.m
+++ psuedo_sweep_FEM_RJ_mod_1.m
@@ -27,24 +27,39 @@ fprintf('Nx x Ny = %i x %i \n', Nx, Ny);
 
 
 %Defining arrays and allocating memory
-u = zeros(Ny, Nx);           % wave field
 x = zeros(Ny, Nx);           % x mesh
 y = zeros(Ny, Nx);           % y mesh
-c = zeros(Ny, Nx);           % wavespeed
-K = zeros(Ny, Nx);           % variable wave number
 I = speye(Ny, Ny);           % Identity matrix
-u_energy = zeros(1, Nx);
 
 for i = 1:Ny
     for j = 1:Nx
         x(i, j) = dx * (j - 1);
-        y(i, j) = -W/2 + dy * (i-1); % centers at (-5, 5)
-        c(i, j) = c0 * (1 - 0.25 * peaks((x(i,j) - W/2) * 10, (y(i,j) - 0.0) * 10) / 8.1006);
-        K(i, j) = OMEGA ./ c(i,j);
+        y(i, j) = -W/2 + dy * (i-1); % centers at (-5, 5)        
     end
 end
 
+c = @(x, y) c0 * (1 - 0.25 * peaks((x - W/2) * 10, (y - 0.0) * 10) / 8.1006);
+K = OMEGA ./ c(x,y);
+
 %%
+
+N = 2;
+VX = linspace(-.5, .5, ceil(Ny / N)+1);
+k = @(x) OMEGA / c(x, 0);
+f = @(x) exp(-x);
+[M, A, b, x, global_to_local_ids] = compute_FE_system(N, VX, k, f);
+
+invM = spdiags(1 ./ spdiags(M), 0, size(M, 1), size(M, 2));
+LB = invM * A;
+
+ndofs = length(x);
+u = zeros(ndofs, Nx);
+u(:, 1) = exp(1i * K(x, 0) .* x);
+
+%%
+
+u = zeros(Ny, Nx);           % wave field
+u_energy = zeros(1, Nx);
 
 %BOUNDARY CONDITION at x=0 -----------------
 u(:,1) = exp(1i * K(:,1) .* x(:,1));
@@ -93,7 +108,7 @@ axis image
 shading interp;
 hcolor = colorbar;
 caxis([-1 1]);
-contour(x,y,c,10,'w','linewidth',1); hold off;
+contour(x,y,c(x,y),10,'w','linewidth',1); hold off;
 view(0,90);
 xlim([0 L]);
 ylim([-W W]/2);
