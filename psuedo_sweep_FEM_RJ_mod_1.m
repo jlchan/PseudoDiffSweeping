7eac31b0c6203f67d6e4160bb797cadb0e5ce051
Jesse Chan <jesse.chan@rice.edu>
2022-06-13 15:18:45 -0500

    fix demo

--- psuedo_sweep_FEM_RJ_mod_1.m
+++ psuedo_sweep_FEM_RJ_mod_1.m
@@ -4,12 +4,12 @@
 
 W = 1;              % Width of domain
 L = 1;              % Length of domain
-OMEGA = 100*pi;     % Angular frequency
+OMEGA = 200*pi;     % Angular frequency
 
 %Determine accuracy 
-PPWx = 20;          % Points per wavelength in x-direction (marching)
-PPWy = 10;          % Points per wavelength in y-direction (tangential)
-ORDER = 2;          % Pseudo-diff order
+PPWx = 30;          % Points per wavelength in x-direction (marching)
+PPWy = 4;          % Points per wavelength in y-direction (tangential)
+ORDER = 4;          % Pseudo-diff order
 
 c0 = 1;                                               % reference wavespeed
 lambda = 2 * pi * c0 / OMEGA;                         % reference wavelength                 
@@ -30,11 +30,11 @@ fprintf('Nx x Ny = %i x %i \n', Nx, Ny);
 
 %%
 
-N = 1;
+N = 5;
 VX = linspace(-.5, .5, ceil(Ny / N)+1);
-k = @(x) OMEGA ./ c(x, 0);
-f = @(x) exp(-x);
-[M, ~, ~, y_FE, global_to_local_ids] = compute_FE_system(N, VX, k, f);
+k = @(x) 1; % dummy argument
+f_zero = @(x) 0;
+[M, ~, ~, y_FE, global_to_local_ids] = compute_FE_system(N, VX, k, f_zero);
 
 %Defining arrays and allocating memory
 Ny = length(y_FE);
@@ -61,26 +61,22 @@ u(:, 1) = exp(1i * K(0, y_FE) .* x(:, 1));
 tic;
 for j=1:Nx-1
     
-    k = @(x) OMEGA ./ c(x_sweeping(j), x);    
-    f = @(x) 0;
-    [M, LB, b, y_FE, global_to_local_ids] = compute_FE_system(N, VX, k, f);
+    k = @(x) K(x_sweeping(j), x);
+    inv_k_sq = @(x) 1 ./ K(x_sweeping(j), x).^2;
+    [~, LB, ~, ~, ~] = compute_FE_system(N, VX, inv_k_sq, f_zero);
     
-    LB = invM * LB;     %overwrite A
-    A = LB;
+    A = invM * LB; 
     
-    % sqrt(1 + LB_operator) = Taylor series
+    % cond(A) = O(1/h^2)
+    % sqrt(1 + A) = sum_i c_o * A^(o-1) 
+    % sqrt(I + A) = I + sum_i (a_i * A) * (I + b_i * A)^{-1}
+    %                            ^^(these terms should be well conditioned)
+        
     DtN = sparse(Ny, Ny);    
     for o = 1:ORDER        
-        DtN = DtN + sqrt_taylor_coeff(o-1) * A^(o-1);   
-    end
-
-%     k_j = k(y_FE); 
-%     A = spdiags(1 ./ k_j, 0, Ny, Ny) * LB * spdiags(1./ k_j, 0, Ny, Ny); 
-%     DtN = sparse(Ny, Ny);
-%     for o = 1:ORDER
-%         DtN = DtN + sqrt_taylor_coeff(o-1) * A^(o-1);
-%     end
-%     DtN = spdiags(sqrt(1i * k_j), 0, Ny, Ny) * DtN * spdiags(sqrt(1i * k_j), 0, Ny, Ny);
+        DtN = DtN + sqrt_taylor_coeff(o-1) * A^(o-1);
+    end    
+    DtN = spdiags(1i * k(y_FE), 0, Ny, Ny) * DtN; % mult by i*k \sum(...)
         
     % Crank-Nicolson
     u(:,j+1) = (I - dx/2 * DtN) \ ((I + dx/2 * DtN) * u(:,j));            
