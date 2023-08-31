ae2a95131c8cf278678ab69c74b49ed1e483a2eb
Jesse Chan <jesse.lee.chan@gmail.com>
2023-08-30 21:58:09 -0500

    got a single iteration working

--- iterative_sweep.jl
+++ iterative_sweep.jl
@@ -63,7 +63,7 @@ function rhs!(du, u, cache, t)
 end
 
 # create FE discretization    
-N = 4
+N = 3
 num_elements = 16
 domain_width, domain_height = 1, 1
 A, y, w1D = assemble_FE_matrix(N, num_elements, domain_height)
@@ -71,8 +71,8 @@ A, y, w1D = assemble_FE_matrix(N, num_elements, domain_height)
 include("radial_solution.jl")
 
 OMEGA = kappa   # Angular frequency, kappa defined in radial_solution.jl
-PPWx  = 20      # Points per wavelength in x-direction (marching)
-ORDER = 4       # Pseudo-diff order
+PPWx  = 10      # Points per wavelength in x-direction (marching)
+ORDER = 3       # Pseudo-diff order
 
 Nx = ceil(Int, domain_width * OMEGA * PPWx)
 Ny = length(y)
@@ -108,17 +108,19 @@ d2udx2_exact(x, y) = -exp(1im * OMEGA * x) * OMEGA^2
 d2udy2_exact(x, y) = zero(eltype(x)) 
 laplacian_u_exact(x, y) = d2udx2_exact(x, y) + d2udy2_exact(x, y)
 forcing = (x, y) -> (OMEGA^2  / c(x, y)^2) * u_exact(x, y) + laplacian_u_exact(x, y)
+
+# radial scattering solution
+function u_exact(x, y)
+    θ = atan.(y, x)
+    r = @. sqrt(x^2 + y^2)
+    return sc(r, θ)
+end
+forcing = (x, y) -> 0 # no manufactured solution (e.g., scattering)
+
 f = forcing.(x', y)
 
-# # radial scattering solution
-# function u_exact(x, y)
-#     θ = atan.(y, x)
-#     r = @. sqrt(x^2 + y^2)
-#     return sc(r, θ)
-# end
-# forcing = (x, y) -> 0 # no manufactured solution (e.g., scattering)
-
-u_initial(x, y) = u_exact(x, y)
+# init as a plane wave 
+u_initial(x, y) = exp(1im * OMEGA * x)
 
 cache = (; OMEGA, ORDER, A, c_data, y, pade_coeffs=pade_coefficients(ORDER))
 
@@ -129,11 +131,23 @@ v = fill!(similar(u), zero(eltype(u)))
 # reference u 
 u_init = u_initial.(x', y)
 
-A_y = (1 / dx^2 ) * spdiagm(0 => 2 * ones(Nx + 1), 1 => -ones(Nx), -1 => -ones(Nx))
-Hu_init = (A * u_init) * A_y'
+# 2nd order 2nd derivative matrix
+A_x = (1 / dx^2) * spdiagm(0 => -2 * ones(Nx + 1), 1 => ones(Nx), -1 => ones(Nx))
+A_x[1, 1:4] .= [2, -5, 4, -1] / dx^2
+A_x[end, end-3:end] .= [-1, 4, -5, 2] / dx^2
 
-residual = f
-# residual = f - Hu_init
+# # 4th order 2nd derivative matrix - to match the order of RK4
+# A_x = (1 / dx^2) * spdiagm(0 => -5/2 * ones(Nx + 1), 
+#                            1 => 4/3 * ones(Nx),  2 => -1/12 * ones(Nx-1), 
+#                           -1 => 4/3 * ones(Nx), -2 => -1/12 * ones(Nx-1))                          
+# A_x[1, 1:6] .= [15/4, -77/6, 107/6, -13, 61/12, -5/6] / dx^2
+# A_x[2, 1:7] .= [0, 15/4, -77/6, 107/6, -13, 61/12, -5/6] / dx^2
+# A_x[end-1, end-6:end] .= reverse([0, 15/4, -77/6, 107/6, -13, 61/12, -5/6] ) / dx^2
+# A_x[end, end-5:end] .= reverse([15/4, -77/6, 107/6, -13, 61/12, -5/6]) / dx^2
+
+# compute (Δu + ω^2 / c^2 * u)
+Hu_init = (A * u_init + u_init * A_x') + (@. (OMEGA^2 / c(x', y)^2) * u_init)
+residual = f - Hu_init
 
 # local storage over a single time slice
 u_tmp, du, du_accum = ntuple(_ -> similar(u[:, 1]), 3)
@@ -145,50 +159,6 @@ end
 
 r_mid = similar(residual[:, 1])
 
-if norm(residual) < 100 * eps()
-    println("Skipping backwards sweep - forcing is zero.")    
-else
-    # backwards sweep to "pick up" the forcing
-    for i in Nx + 1:-1:2
-        fill!(du_accum, zero(eltype(du_accum)))
-
-        # interpolate to midpoint between x[i-1] and x[i]
-        if i > 3 # < Nx - 2
-            @. r_mid = 0.0625 * residual[:, i-3] - 0.3125 * residual[:, i-2] + 0.9375 * residual[:, i-1] + 0.3125 * residual[:, i]
-        else # if i==2
-            @. r_mid = 0.3125 * residual[:, i-1] + 0.9375  * residual[:, i] - 0.3125 * residual[:, i+1] +  0.0625 * residual[:, i+2]
-        end
-        
-        rhs!(du, v[:,i], cache, x[i]) 
-        @. du = du - residual[:, i] 
-        @. du_accum += du
-
-        @. u_tmp = v[:,i] + 0.5 * dx * du
-        impose_homogeneous_BCs!(u_tmp)
-        rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
-        @. du = du - r_mid
-        @. du_accum += 2 * du
-
-        @. u_tmp = v[:,i] + 0.5 * dx * du
-        impose_homogeneous_BCs!(u_tmp)
-        rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
-        @. du = du - r_mid
-        @. du_accum += 2 * du
-
-        @. u_tmp = v[:,i] + dx * du
-        impose_homogeneous_BCs!(u_tmp)
-        rhs!(du, u_tmp, cache, x[i-1]) 
-        @. du = du - residual[:, i-1]
-        @. du_accum += du
-        
-        @. v[:,i-1] = v[:,i] + (dx / 6) * du_accum
-        impose_homogeneous_BCs!(view(v, :, i-1))
-        if i % 100 == 0
-            println("On step $i out of $(Nx-1)")
-        end
-    end
-end
-
 # temp memory for interpolation
 v_mid = similar(v[:,1])
 fill!(u, zero(eltype(u)))
@@ -196,12 +166,54 @@ fill!(u, zero(eltype(u)))
 u_left(t) = u_exact(t, 0.5)
 u_right(t) = u_exact(t, -0.5)
 function impose_BCs!(u, t)
-    u[1] = u_left(t)
-    u[end] = u_right(t)
+    u[1] = 0
+    u[end] = 0
+    # u[1] = u_left(t)
+    # u[end] = u_right(t)
 end
 
-u[:, 1] .= u_init[:, 1]
 
+# backwards sweep to "pick up" the forcing
+for i in Nx + 1:-1:2
+    fill!(du_accum, zero(eltype(du_accum)))
+
+    # interpolate to midpoint between x[i-1] and x[i]
+    if i > 3 # < Nx - 2
+        @. r_mid = 0.0625 * residual[:, i-3] - 0.3125 * residual[:, i-2] + 0.9375 * residual[:, i-1] + 0.3125 * residual[:, i]
+    else # if i==2
+        @. r_mid = 0.3125 * residual[:, i-1] + 0.9375  * residual[:, i] - 0.3125 * residual[:, i+1] +  0.0625 * residual[:, i+2]
+    end
+    
+    rhs!(du, v[:,i], cache, x[i]) 
+    @. du = du - residual[:, i] 
+    @. du_accum += du
+
+    @. u_tmp = v[:,i] + 0.5 * dx * du
+    impose_homogeneous_BCs!(u_tmp)
+    rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
+    @. du = du - r_mid
+    @. du_accum += 2 * du
+
+    @. u_tmp = v[:,i] + 0.5 * dx * du
+    impose_homogeneous_BCs!(u_tmp)
+    rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
+    @. du = du - r_mid
+    @. du_accum += 2 * du
+
+    @. u_tmp = v[:,i] + dx * du
+    impose_homogeneous_BCs!(u_tmp)
+    rhs!(du, u_tmp, cache, x[i-1]) 
+    @. du = du - residual[:, i-1]
+    @. du_accum += du
+    
+    @. v[:,i-1] = v[:,i] + (dx / 6) * du_accum
+    impose_homogeneous_BCs!(view(v, :, i-1))
+    if i % 100 == 0
+        println("On step $i out of $(Nx-1)")
+    end
+end
+
+# forward sweep to incorporate v
 for i in 1:Nx
 
     # interpolate to midpoint between x[i] and x[i+1]
@@ -243,12 +255,12 @@ for i in 1:Nx
     end
 end
 
-# # add background u
+# # # add background u
 # u += u_init
 
 uex = u_exact.(x', y) 
-# uex[@. sqrt((x')^2 + y^2) < R] .= NaN
-error = u - u_exact.(x', y)
+uex[@. sqrt((x')^2 + y^2) < R] .= NaN
+error = (u_init + u) - u_exact.(x', y)
 w = repeat(w1D, 1, Nx+1)
 w = domain_width * domain_height * w / sum(w) 
 L2_error = sqrt(dot(w, @. abs(error)^2))
@@ -260,8 +272,8 @@ L2_error = sqrt(dot(w, @. abs(error)^2))
 
 using Plots: Plots
 
-# p1 = Plots.contourf(x, y, real.(u), c=:viridis, clims=(-4, 4), leg=false, ratio=1, title="Sweeping")
-# p2 = Plots.contourf(x, y, real.(uex), c=:viridis,  clims=(-4, 4), leg=false, ratio=1, title="Analytical")
-# Plots.plot(p1, p2)
+p1 = Plots.contourf(x, y, real.(u_init + u), c=:viridis, clims=(-4, 4), leg=false, ratio=1, title="Sweeping")
+p2 = Plots.contourf(x, y, real.(uex), c=:viridis,  clims=(-4, 4), leg=false, ratio=1, title="Analytical")
+Plots.plot(p1, p2)
 
-Plots.contourf(x, y, abs.(uex-u), c=:viridis, leg=false, ratio=1, title="Error", colorbar=true)
+# Plots.contourf(x, y, abs.(uex-u), c=:viridis, leg=false, ratio=1, title="Error", colorbar=true)
