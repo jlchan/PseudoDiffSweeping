73a5bdeb008985cce227605dc65563458df59531
Jesse Chan <jesse.lee.chan@gmail.com>
2023-08-30 20:05:27 -0500

    incorporating the radial solution

--- pseudo_sweep.jl
+++ pseudo_sweep.jl
@@ -63,26 +63,28 @@ function rhs!(du, u, cache, t)
 end
 
 # create FE discretization    
-N = 3
+N = 4
 num_elements = 16
 domain_width, domain_height = 1, 1
 A, y, w1D = assemble_FE_matrix(N, num_elements, domain_height)
 
-OMEGA = 40           # Angular frequency
-PPWx  = 10            # Points per wavelength in x-direction (marching)
-ORDER = 3            # Pseudo-diff order
+include("radial_solution.jl")
+
+OMEGA = kappa   # Angular frequency, kappa defined in radial_solution.jl
+PPWx  = 20      # Points per wavelength in x-direction (marching)
+ORDER = 4       # Pseudo-diff order
 
 Nx = ceil(Int, domain_width * OMEGA * PPWx)
 Ny = length(y)
 x = LinRange(-0.5, -0.5 + domain_width, Nx + 1)
 dx = x[2] - x[1]
 
-peaks(x, y) = 3 * (1-x)^2 * exp(-(x.^2) - (y+1).^2) - 
-              10 * (x / 5 - x^3 - y^5) * exp(-x^2 - y^2) - 
-              1/3 * exp(-(x+1)^2 - y^2) 
+# peaks(x, y) = 3 * (1-x)^2 * exp(-(x.^2) - (y+1).^2) - 
+#               10 * (x / 5 - x^3 - y^5) * exp(-x^2 - y^2) - 
+#               1/3 * exp(-(x+1)^2 - y^2) 
 
 # one_minus_b = (x,y) -> 1 + 1.5 * exp(-160 * ((x-0.5)^2 + y^2))
-b(x,y) = b(sqrt(x^2 + y^2)) # b should be given by "radial solution"
+b(x,y) = b(sqrt(x^2 + y^2)) # b should be given by "radial_solution.jl"
 one_minus_b = (x,y) -> 1 - b(x,y) # OMEGA^2 * (1 - b(x)) <---> OMEGA^2 / c^2
 c(x,y) = 1 / sqrt(one_minus_b(x, y)) # (1 - b(x)) = 1/c^2 ------> c = 1 / sqrt(1-b(x))
 d_inv_c2(x, y) = ForwardDiff.derivative(x -> 1 / c(x, y)^2, x)
@@ -100,15 +102,16 @@ end
 
 # manufactured plane wave solution
 u_exact(x, y) = exp(1im * OMEGA * x)
+u_initial(x, y) = u_exact(x, y)
+
+# manufactured plane wave solution variables
 dudx_exact(x, y) = ForwardDiff.derivative(x->u_exact(x, y), x)
 dudy_exact(x, y) = ForwardDiff.derivative(y->u_exact(x, y), y)
 laplacian_u_exact(x, y) = ForwardDiff.derivative(x->dudx_exact(x, y), x) + ForwardDiff.derivative(y->dudy_exact(x, y), y)
-
 forcing = (x, y) -> (OMEGA^2  / c(x, y)^2) * u_exact(x, y) + laplacian_u_exact(x, y)
-u_initial(x, y) = u_exact(x, y)
 f = forcing.(x', y)
 
-forcing = (x, y) -> 0 # no manufactured solution
+# forcing = (x, y) -> 0 # no manufactured solution (e.g., scattering)
 
 cache = (; OMEGA, ORDER, A, c_data, y, pade_coeffs=pade_coefficients(ORDER))
 
@@ -116,10 +119,11 @@ cache = (; OMEGA, ORDER, A, c_data, y, pade_coeffs=pade_coefficients(ORDER))
 u = zeros(Complex{eltype(y)}, Ny, Nx + 1)
 u_tmp, du, du_accum = ntuple(_ -> similar(u[:, 1]), 3)
 
-function impose_homogeneous_BCs!(u)
+function impose_homogeneous_BCs!(u, args...)
     # u[1] = zero(eltype(u))
     # u[end] = zero(eltype(u))
 end
+
 
 if norm(forcing.(x', y)) < 100 * eps()
     println("Skipping backwards sweep - forcing is zero.")    
@@ -164,14 +168,13 @@ u_backwards = copy(u)
 u_backwards_mid = similar(u_backwards[:,1])
 fill!(u, zero(eltype(u)))
 
-function u_exact(x, y)
-    θ = atan.(y, x)
-    r = @. sqrt(x^2 + y^2)
-    return sc(r, θ)
-end
-u_initial(x, y) = u_exact(x, y)
+# function u_exact(x, y)
+#     θ = atan.(y, x)
+#     r = @. sqrt(x^2 + y^2)
+#     return sc(r, θ)
+# end
+# u_initial(x, y) = u_exact(x, y)
 
-# should by symmetric anyways
 u_left(t) = u_exact(t, 0.5)
 u_right(t) = u_exact(t, -0.5)
 function impose_BCs!(u, t)
@@ -220,41 +223,6 @@ for i in 1:Nx
     end
 end
 
-# u_forward = copy(u)
-
-# # backwards sweep to "pick up" the forcing
-# for i in Nx + 1:-1:2
-#     fill!(du_accum, zero(eltype(du_accum)))
-    
-#     rhs!(du, u[:,i], cache, x[i]) 
-#     @. du = du - u_forward[:,i]
-#     @. du_accum += du
-
-#     @. u_tmp = u[:,i] + 0.5 * dx * du
-#     impose_BCs!(u_tmp, x[i] - 0.5 * dx)
-#     rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
-#     @. du = du - 0.5 * (u_forward[:,i] + u_forward[:,i-1])
-#     @. du_accum += 2 * du
-
-#     @. u_tmp = u[:,i] + 0.5 * dx * du
-#     impose_BCs!(u_tmp, x[i] - 0.5 * dx)
-#     rhs!(du, u_tmp, cache, x[i] - 0.5 * dx) 
-#     @. du = du - 0.5 * (u_forward[:,i] + u_forward[:,i-1])
-#     @. du_accum += 2 * du
-
-#     @. u_tmp = u[:,i] + dx * du
-#     impose_BCs!(u_tmp, x[i-1])
-#     rhs!(du, u_tmp, cache, x[i-1]) 
-#     @. du = du - u_forward[:,i-1]
-#     @. du_accum += du
-    
-#     @. u[:,i-1] = u[:,i] + (dx / 6) * du_accum
-#     impose_BCs!(u_tmp, x[i-1])
-#     if i % 100 == 0
-#         println("On step $i out of $(Nx-1)")
-#     end
-# end
-
 uex = u_exact.(x', y) 
 uex[@. sqrt((x')^2 + y^2) < R] .= NaN
 error = u - u_exact.(x', y)
@@ -271,6 +239,8 @@ using Plots: Plots
 
 p1 = Plots.contourf(x, y, real.(u), c=:viridis, clims=(-4, 4), leg=false, ratio=1, title="Sweeping")
 p2 = Plots.contourf(x, y, real.(uex), c=:viridis,  clims=(-4, 4), leg=false, ratio=1, title="Analytical")
-plot(p1, p2)
+Plots.plot(p1, p2)
+
+# Plots.contourf(x, y, real.(uex-u), c=:viridis, clims=(-.05, .05), leg=false, ratio=1, title="Sweeping", colorbar=true)
 
 # Plots.contourf(x, y, abs.(u .- uex), c=:viridis, leg=false, ratio=1, colorbar=true, title="Error")
